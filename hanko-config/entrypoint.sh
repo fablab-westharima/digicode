0d99c174dd60549671e1ba5ef2681d9fe2693e46
Kazunari Takeda <kazunari.takeda@digi-co.jp>
2025-12-12 11:25:17 +0900

    fix(hanko): Use direct CMD instead of entrypoint script (distroless compatibility)

--- hanko-config/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Running Hanko database migrations..."
-hanko migrate up --config /etc/hanko/config.yaml
-
-echo "Starting Hanko server..."
-exec hanko serve public --config /etc/hanko/config.yaml
